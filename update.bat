44334eaa76ecb747b3ec76fea573291c8a6aebda
Alexis Peppas <apeppas@outlook.com>
2017-05-15 17:33:54 -0700

    updates

--- update.bat
+++ update.bat
@@ -3,9 +3,6 @@ pdflatex thesis.tex
 bibtex thesis.aux
 pdflatex thesis.tex
 pdflatex thesis.tex
-git add .
-git commit -m "updates"
-git push -u origin master
 cd ..
 "C:\Program Files\7-Zip\7z.exe" a -r thesis.zip -w Thesis
 cd Thesis
